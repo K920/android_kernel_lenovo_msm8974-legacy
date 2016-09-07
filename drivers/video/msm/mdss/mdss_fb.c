96f59516ac024bb2f844ffb46ea5a7e7fa8a58f3
Michal Chvíla <electrydev@gmail.com>
2016-09-07 18:22:52 +0200

    video: mdss: lcd_effect: Rewrite everything from scratch
    Stock lcd_effect was a bit messy, now at least it's easy to figure
    out what does what.
    The struct names are now pretty straightforward and there is minimal
    confusion.
    
    Change-Id: Ie6a163c3191f38f76e3e1e834b50c39a7b45c59c
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -57,6 +57,10 @@
 
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_FB_MSM_MDSS_LCD_EFFECT
+#include "mdss_lcd_effect.h"
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -543,7 +547,12 @@ static int mdss_fb_create_sysfs(struct msm_fb_data_type *mfd)
 	if (rc)
 		pr_err("sysfs group creation failed, rc=%d\n", rc);
 
-	return mdss_livedisplay_create_sysfs(mfd);
+	rc = mdss_livedisplay_create_sysfs(mfd);
+#ifdef CONFIG_FB_MSM_MDSS_LCD_EFFECT
+	rc = mdss_lcd_effect_create_sysfs(mfd);
+#endif
+
+	return rc;
 }
 
 static void mdss_fb_remove_sysfs(struct msm_fb_data_type *mfd)
